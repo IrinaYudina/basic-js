493427e99d72a50813dc6de2cdb6a3f0a160c32f
IrinaYudina <60599726+IrinaYudina@users.noreply.github.com>
2020-09-10 10:12:41 +0400

    add the functionality

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,15 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(date) {
+  if(typeof date === 'string') {
+    let formatString = date.substring(0, 3) + date.substring(3, date.length).split('.').join('');
+    if(!isNaN(Number(formatString)) && Number(formatString) < MODERN_ACTIVITY && Number(formatString) > 0){
+      let res = Number(formatString);
+      let k = 0.693/HALF_LIFE_PERIOD;
+      res = Math.ceil(Math.log(MODERN_ACTIVITY/res)/k);
+      return res;
+    }
+  }
+  return false;
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let res = [];
+  if(!Array.isArray(arr)){
+    throw new Error();
+  }
+  else{
+  for( let i = 0; i < arr.length; i++) {
+    let s = res.length;
+    if(arr[i] === '--discard-next') {
+      if(i != arr.length - 1) { 
+        i = i + 1;
+      }
+      continue;
+    }
+    else if(arr[i] === '--discard-prev') {
+      if(i != 0) {
+        if(arr.length >= 3 && arr[i - 2] != '--discard-next' && i >= 2){
+          res.splice(-1, 1)
+        }
+        else if( i == 1){
+          res.splice(-1, 1)
+        }
+      }
+    }
+    else if(arr[i] === '--double-next') {
+      if(i != arr.length - 1) {
+        res.push(arr[i + 1]);
+      }
+    }
+    else if(arr[i] === '--double-prev') {
+      if(i != 0) {
+        if(arr.length >= 3 && arr[i - 2] != '--discard-next' && i >= 2){
+          res.push(arr[i - 1]);
+        }
+        else if( i == 1){
+          res.push(arr[i - 1]);
+        }
+      }
+    }
+    else{
+      res.push(arr[i]);
+    }
+  }
+}
+return res;
 };
